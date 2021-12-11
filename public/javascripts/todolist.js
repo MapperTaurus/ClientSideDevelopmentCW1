a3da94f7614ebb0b3d159b8c53d5876e03065a85
Ivan Todorov <64197668+MapperTaurus@users.noreply.github.com>
2021-12-11 14:03:22 +0000

    Add files via upload

--- public/javascripts/todolist.js
+++ public/javascripts/todolist.js
@@ -34,7 +34,7 @@ async function showlist(data)
     // for each element in the data
     for (const item of data) {
         // create an object to represent the current item
-        let value = new ToDoItem(item.id, item.name, item.address, item.postcode, item.phone, item.email, item.description + item.ratings.toString() + JSON.stringify(item.reviews) );
+        let value = new ToDoItem(item.id, item.name, item.address, item.postcode, item.phone, item.email, item.description);
 
          // create LI element
         const node = document.createElement("li");     
@@ -45,14 +45,79 @@ async function showlist(data)
         }
 
         // create check box node and append to list item
-        const checkboxnode = document.createElement("input");
-        checkboxnode.setAttribute("type", "checkbox");
-        checkboxnode.setAttribute("name", "markdone");
-        node.appendChild(checkboxnode);
-        
+        const name_icon = document.createElement("i");
+        name_icon.setAttribute("class", "fas fa-h-square");
+        const name_icon_br = document.createElement("br");
+
+        const address_icon = document.createElement("i");
+        address_icon.setAttribute("class", "fas fa-map-marker-alt");
+        const address_br = document.createElement("br");
+
+        const postcode_icon = document.createElement("i");
+        postcode_icon.setAttribute("class", "fas fa-thumbtack");
+        const postcode_br = document.createElement("br");
+
+        const phone_icon = document.createElement("i");
+        phone_icon.setAttribute("class", "fas fa-phone");
+        const phone_br = document.createElement("br");
+
+        const email_icon = document.createElement("i");
+        email_icon.setAttribute("class", "fas fa-at");
+        const email_br = document.createElement("br");
+
+        const description_icon = document.createElement("i");
+        description_icon.setAttribute("class", "fas fa-info-circle");
+        const description_br = document.createElement("br");
+
+        const ratings_icon = document.createElement("i");
+        ratings_icon.setAttribute("class", "fas fa-star");
+        const ratings_br = document.createElement("br");
+
+        const reviews_icon = document.createElement("i");
+        reviews_icon.setAttribute("class", "fas fa-comments");
+        const reviews_br = document.createElement("p");
+
         // create text node and append to list item
-        const textnode = document.createTextNode(value.id + value.name + value.address + value.postcode + value.phone + value.email + value.description + item.ratings.toString() + JSON.stringify(item.reviews));         
-        node.appendChild(textnode);  
+
+        const textnode_id = document.createTextNode("ID:" + value.id);  
+        const textnode_name = document.createTextNode("Name:" + value.name);
+        const textnode_address = document.createTextNode("Address:" + value.address);   
+        const textnode_postcode = document.createTextNode("Postcode:" + value.postcode);
+        const textnode_phone= document.createTextNode("Phone:" + value.phone);      
+        const textnode_email= document.createTextNode("Email:" + value.email); 
+        const textnode_description= document.createTextNode("Description:" + value.description);    
+        const textnode_ratings= document.createTextNode("Ratings:" + item.ratings.toString());   
+        const textnode_reviews= document.createTextNode("Reviews:" + JSON.stringify(item.reviews, null, 1));  
+
+        //node.appendChild(textnode_id); 
+        
+        node.appendChild(name_icon);
+        node.appendChild(textnode_name);
+        node.appendChild(name_icon_br);
+        node.appendChild(address_icon);
+        node.appendChild(textnode_address);
+        node.appendChild(address_br);
+        node.appendChild(postcode_icon);
+        node.appendChild(textnode_postcode);
+        node.appendChild(postcode_br);
+        node.appendChild(phone_icon);
+        node.appendChild(textnode_phone);
+        node.appendChild(phone_br);
+        node.appendChild(email_icon);
+        node.appendChild(textnode_email);
+        node.appendChild(email_br);
+        node.appendChild(description_icon);
+        node.appendChild(textnode_description);
+        node.appendChild(description_br);
+        node.appendChild(ratings_icon);
+        node.appendChild(textnode_ratings);
+        node.appendChild(ratings_br);
+        node.appendChild(reviews_icon);
+        node.appendChild(textnode_reviews);
+        node.appendChild(reviews_br);
+
+    
+         
 
         // append node to list
         hostels.appendChild(node);
